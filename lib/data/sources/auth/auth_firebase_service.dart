18308e2e9d352eb5b8be57f0a793cb7f2441d677
chaaanuwu <gkchanuwa@gmail.com>
2026-08-27 06:50:10 +0530

    Implement authentication feature with signup and signin functionality

---
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify_clone/data/models/auth/create_user_req.dart';
+
+abstract class AuthFirebaseService {
+  Future<void> signin();
+  Future<void> signup(CreateUserReq createUserReq);
+}
+
+class AuthFirebaseServiceImpl implements AuthFirebaseService {
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup(CreateUserReq createUserReq) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: createUserReq.email, password: createUserReq.password);
+    } on FirebaseAuthException catch(e) {
+
+    }
+  }
+}
